99d4000273f1f68f645ab132847beedd5c2e8e2d
Matias Totaro <matiastotaro@gmail.com>
2026-05-17 00:28:49 -0300

    fix: suppressHydrationWarning on body (browser extension attr injection)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -54,7 +54,7 @@ export default async function LocaleLayout({
       lang={locale}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-background text-foreground">
+      <body className="min-h-full flex flex-col bg-background text-foreground" suppressHydrationWarning>
         <NextIntlClientProvider messages={messages}>
           <Nav locale={locale} />
           {children}
